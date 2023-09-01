5294d02833ee713baa698e4f92b6fe1b7c457dfa
AnaDaniela96 <108939830+AnaDaniela96@users.noreply.github.com>
2023-08-31 22:01:57 -0600

    Vistas para productos y servicios
    Este commit agrega los cambios para tener las rutas de productos y servicios.

---
+++ src/app/pages/capacitacion/arte-latte/arte-latte.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-arte-latte',
+  templateUrl: './arte-latte.component.html',
+  styleUrls: ['./arte-latte.component.css']
+})
+export class ArteLatteComponent {
+
+}
