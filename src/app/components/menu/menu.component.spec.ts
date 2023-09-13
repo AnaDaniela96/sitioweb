2aef09681b001cb458a83b7425472905f7428bd7
Héctor Hugo Rivera González <sistemas@cafeetrusca.com>
2023-09-13 17:02:59 -0600

    Se actualiza menu y elementos del foother
    Se actualiza menu y elementos del foother

--- src/app/components/menu/menu.component.spec.ts
+++ src/app/components/menu/menu.component.spec.ts
@@ -1,5 +1,4 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
-
 import { MenuComponent } from './menu.component';
 
 describe('MenuComponent', () => {
@@ -17,5 +16,27 @@ describe('MenuComponent', () => {
 
   it('should create', () => {
     expect(component).toBeTruthy();
+  });
+
+  it('should set isMenuFixed to true when scrolling past 100 pixels', () => {
+    // Simula un evento de desplazamiento con una posición mayor a 100 pixels
+    spyOnProperty(window, 'pageYOffset').and.returnValue(101);
+
+    // Dispara el evento de desplazamiento manualmente
+    window.dispatchEvent(new Event('scroll'));
+
+    // Verifica si isMenuFixed se estableció en true
+    expect(component.isMenuFixed).toBe(true);
+  });
+
+  it('should set isMenuFixed to false when scrolling less than 100 pixels', () => {
+    // Simula un evento de desplazamiento con una posición menor a 100 pixels
+    spyOnProperty(window, 'pageYOffset').and.returnValue(99);
+
+    // Dispara el evento de desplazamiento manualmente
+    window.dispatchEvent(new Event('scroll'));
+
+    // Verifica si isMenuFixed se estableció en false
+    expect(component.isMenuFixed).toBe(false);
   });
 });
