3a13009ca33161a7827950953968f3077b828f04
Héctor Hugo Rivera González <sistemas@cafeetrusca.com>
2023-09-19 09:33:00 -0600

    Se agrega Theme de calabaza y WhatsApp
    Se agrega Theme de calabaza y WhatsApp

--- src/app/pages/home/home.component.ts
+++ src/app/pages/home/home.component.ts
@@ -1,12 +1,14 @@
 import { Component } from '@angular/core';
-declare var __moduleName: any;
+import * as AOS from 'aos';
+
 @Component({
-  moduleId:__moduleName,
   selector: 'app-home',
   templateUrl: './home.component.html',
   styleUrls: ['home.component.css'],
   styles: ['h1 { font-weight: normal; color : red !important; }']
 })
-export class HomeComponent {
-
+export class HomeComponent  {
+    ngOnInit() {
+     AOS.init();
+   }
 }
