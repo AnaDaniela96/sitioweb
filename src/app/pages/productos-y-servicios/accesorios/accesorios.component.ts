2fcd737ef8b927c59e6766a219f5656d5c811ac9
AnaDaniela96 <108939830+AnaDaniela96@users.noreply.github.com>
2023-09-21 14:43:36 -0600

    Agrega menú de categorías para accesorios.

--- src/app/pages/productos-y-servicios/accesorios/accesorios.component.ts
+++ src/app/pages/productos-y-servicios/accesorios/accesorios.component.ts
@@ -6,5 +6,44 @@ import { Component } from '@angular/core';
   styleUrls: ['./accesorios.component.css']
 })
 export class AccesoriosComponent {
+  categorias = {
+    metodoGoteo: false,
+    prensaFrancesa: false,
+    jarrasEspumadoras: false, 
+    tampers: false,
+    molinos: false,
+    catacion: false,
+    vasos: false,
+    cafeteria: false,
+    metodosExtraccion: false,
+    tesYteteras: false,
+  };
 
+  subcategorias = {
+    metodoGoteo: {
+      filtros: false,
+      drippers: false, 
+    },
+
+    cafeteria: {
+      especieros: false,
+      tazasVidrio: false,
+      bagaceras: false,
+      sifones: false,
+      elementosAuxiliares: false,
+    },
+
+    metodosExtraccion: {
+      filtros: false,
+      complementosMetodos: false,
+      limpieza: false,
+      kitsParaPruebas: false,
+      jarrasServidoras: false,
+    }
+    // prensaFrancesa: {
+    //   vidrioMadera: false,
+    //   subcategoria2: false
+    // }
+    // Puedes agregar más subcategorías según tus necesidades
+  };
 }
