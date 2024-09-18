a7dac6d5e2619c4d1fc3d1edf3d318673ef1f4e0
AnaDaniela96 <108939830+AnaDaniela96@users.noreply.github.com>
2024-09-18 12:51:27 -0600

    update polvo module

--- src/app/pages/productos-y-servicios/insumos/polvos/polvos.component.ts
+++ src/app/pages/productos-y-servicios/insumos/polvos/polvos.component.ts
@@ -1,7 +1,6 @@
-import { Component } from '@angular/core';
+import { Component, AfterViewInit, QueryList, ViewChildren } from '@angular/core';
 import { DataService } from '../../../../utils/data.service';
 import { DomSanitizer, SafeUrl } from '@angular/platform-browser';
-
 
 @Component({
   selector: 'app-polvos',
@@ -18,30 +17,344 @@ export class PolvosComponent {
   insumosArray: any[] = [];
   extractedUrls: any;
 
-   //Polvos
-   polvosChillOutMix: any[] = [];
-   polvosChillOutJustFruit: any[] = [];
-   polvosChillOutActive: any[] = [];
-   polvosDavidRio: any[] = [];
-   polvosSuperBlends: any[] = [];
-   polvosTeaZone: any[] = [];
-   polvosAiya:  any[] = [];
-   polvosDonGustavo:  any[] = [];
-   polvosMocafe:  any[] = [];
-   polvosCoolCap:  any[] = [];
+  //Menu
+  hidesChillOutMix: boolean = true;
+  hidesChillOutJustFruit: boolean = true;
+  hidesChillOutActive: boolean = true;
+  hidesDavidRio: boolean = true;
+  hidesSuperBlends: boolean = true;
+  hidesTeaZone: boolean = true;
+  hidesAiya: boolean = true;
+  hidesDonGustavo: boolean = true;
+  hidesMocafe: boolean = true;
+  hidesCoolCap: boolean = true;
+  hidesCappuccinee: boolean = true;
+  hidesHollander: boolean = true;
 
-  hidesChillOutMix : boolean = true;
-  hidesChillOutJustFruit : boolean = true;
-  hidesChillOutActive : boolean = true;
-  hidesDavidRio : boolean = true;
-  hidesSuperBlends : boolean = true;
-  hidesTeaZone: boolean = false;
-  hidesAiya : boolean = false;
-  hidesDonGustavo : boolean = false;   
-  hidesMocafe : boolean = false;
-  hidesCoolCap : boolean = false;
+  mostrarCuadricula: boolean = false;
+  elementosVisibles: number = 0;
 
-   ngOnInit(): void {
+  polvosSuperBlends: any [] = [];
+  polvosChillOutActive: any [] = [];
+
+  // Polvos ChillOutMix
+  polvosChillOutMix: any [] = [];
+  paginaActualChillOutMix: number = 1;
+  tamañoPaginaChillOutMix: number = 5;
+
+  // Polvos ChillOut Just Fruit
+  polvosChillOutJustFruit: any [] = [];
+  paginaActualChillOutJustFruit: number = 1;
+  tamañoPaginaChillOutJustFruit: number = 5;
+
+  // Polvos Tea Zone
+  polvosTeaZone: any [] = [];
+  paginaActualTeaZone: number = 1;
+  tamañoPaginaTeaZone: number = 5;
+
+  // Polvos AIYA
+  polvosAiya: any [] = [];
+  paginaActualAiya: number = 1;
+  tamañoPaginaAiya: number = 5;
+
+  // Polvos Don Gustavo
+  polvosDonGustavo: any [] = [];
+  paginaActualDonGustavo: number = 1;
+  tamañoPaginaDonGustavo: number = 5;
+
+  // Polvos David Rio
+  polvosDavidRio: any [] = [];
+  paginaActualDavidRio: number = 1;
+  tamañoPaginaDavidRio: number = 5;
+
+  // Polvos Mocafe
+  polvosMocafe: any [] = [];
+  paginaActualMocafe: number = 1;
+  tamañoPaginaMocafe: number = 5;
+
+  // Polvos CoolCapp
+  polvosCoolCap: any [] = [];
+  paginaActualCoolCap: number = 1;
+  tamañoPaginaCoolCap: number = 5;
+
+  // Polvos Cappuccinee
+  polvosCappucchinee: any [] = [];
+  paginaActualCappuccine: number = 1;
+  tamañoPaginaCappuccine: number = 5;
+
+  // Polvos Hollander
+  polvosHollander: any [] = [];
+  paginaActualHollander: number = 1;
+  tamañoPaginaHollander: number = 5;
+
+  @ViewChildren('tarjeta')
+  tarjetas!: QueryList<any>;
+
+  ngAfterViewInit() {
+    this.elementosVisibles = this.tarjetas.length;
+  }
+
+  // Obtener los elementos de la página actual para ChillOut Mix
+  get elementosPaginaChillOutMix() {
+    const inicio = (this.paginaActualChillOutMix - 1) * this.tamañoPaginaChillOutMix;
+    const fin = inicio + this.tamañoPaginaChillOutMix;
+    return this.polvosChillOutMix.slice(inicio, fin);
+  }
+
+  // Obtener las páginas para ChillOut Mix
+  get paginasChillOutMix() {
+    const totalPaginas = Math.ceil(this.polvosChillOutMix.length / this.tamañoPaginaChillOutMix);
+    return Array(totalPaginas).fill(0).map((_, index) => index + 1);
+  }
+
+  // Cambiar página para Café Etrusca
+  paginaAnteriorChillOutMix() {
+    if (this.paginaActualChillOutMix > 1) {
+      this.paginaActualChillOutMix--;
+    }
+  }
+
+  paginaSiguienteChillOutMix() {
+    if (this.paginaActualChillOutMix < this.paginasChillOutMix.length) {
+      this.paginaActualChillOutMix++;
+    }
+  }
+
+  // Obtener los elementos de la página actual para ChillOut Just Fruit
+  get elementosPaginaChillOutJustFruit() {
+    const inicio = (this.paginaActualChillOutJustFruit - 1) * this.tamañoPaginaChillOutJustFruit;
+    const fin = inicio + this.tamañoPaginaChillOutJustFruit;
+    return this.polvosChillOutJustFruit.slice(inicio, fin);
+  }
+
+  // Obtener las páginas para ChillOut Just Fruit
+  get paginasChillOutJustFruit() {
+    const totalPaginas = Math.ceil(this.polvosChillOutJustFruit.length / this.tamañoPaginaChillOutJustFruit);
+    return Array(totalPaginas).fill(0).map((_, index) => index + 1);
+  }
+
+  // Cambiar página para ChillOutJustFruit
+  paginaAnteriorChillOutJustFruit() {
+    if (this.paginaActualChillOutJustFruit > 1) {
+      this.paginaActualChillOutJustFruit--;
+    }
+  }
+
+  paginaSiguienteChillOutJustFruit() {
+    if (this.paginaActualChillOutJustFruit < this.paginasChillOutJustFruit.length) {
+      this.paginaActualChillOutJustFruit++;
+    }
+  }
+
+  // Obtener los elementos de la página actual para TeaZone
+  get elementosPaginaTeaZone() {
+    const inicio = (this.paginaActualTeaZone - 1) * this.tamañoPaginaTeaZone;
+    const fin = inicio + this.tamañoPaginaTeaZone;
+    return this.polvosTeaZone.slice(inicio, fin);
+  }
+
+  // Obtener las páginas para TeaZone
+  get paginasTeaZone() {
+    const totalPaginas = Math.ceil(this.polvosTeaZone.length / this.tamañoPaginaTeaZone);
+    return Array(totalPaginas).fill(0).map((_, index) => index + 1);
+  }
+
+  // Cambiar página para TeaZone
+  paginaAnteriorTeaZone() {
+    if (this.paginaActualTeaZone > 1) {
+      this.paginaActualTeaZone--;
+    }
+  }
+
+  paginaSiguienteTeaZone() {
+    if (this.paginaActualTeaZone < this.paginasTeaZone.length) {
+      this.paginaActualTeaZone++;
+    }
+  }
+
+  // Obtener los elementos de la página actual para Aiya
+  get elementosPaginaAiya() {
+    const inicio = (this.paginaActualAiya - 1) * this.tamañoPaginaAiya;
+    const fin = inicio + this.tamañoPaginaAiya;
+    return this.polvosAiya.slice(inicio, fin);
+  }
+
+  // Obtener las páginas para Aiya
+  get paginasAiya() {
+    const totalPaginas = Math.ceil(this.polvosAiya.length / this.tamañoPaginaAiya);
+    return Array(totalPaginas).fill(0).map((_, index) => index + 1);
+  }
+
+  // Cambiar página para Aiya
+  paginaAnteriorAiya() {
+    if (this.paginaActualAiya > 1) {
+      this.paginaActualAiya--;
+    }
+  }
+
+  paginaSiguienteAiya() {
+    if (this.paginaActualAiya < this.paginasAiya.length) {
+      this.paginaActualAiya++;
+    }
+  }
+
+  // Obtener los elementos de la página actual para Don Gustavo
+  get elementosPaginaDonGustavo() {
+    const inicio = (this.paginaActualDonGustavo - 1) * this.tamañoPaginaDonGustavo;
+    const fin = inicio + this.tamañoPaginaDonGustavo;
+    return this.polvosDonGustavo.slice(inicio, fin);
+  }
+
+  // Obtener las páginas para Don Gustavo
+  get paginasDonGustavo() {
+    const totalPaginas = Math.ceil(this.polvosDonGustavo.length / this.tamañoPaginaDonGustavo);
+    return Array(totalPaginas).fill(0).map((_, index) => index + 1);
+  }
+
+  // Cambiar página para Don Gustavo
+  paginaAnteriorDonGustavo() {
+    if (this.paginaActualDonGustavo > 1) {
+      this.paginaActualDonGustavo--;
+    }
+  }
+
+  paginaSiguienteDonGustavo() {
+    if (this.paginaActualDonGustavo < this.paginasDonGustavo.length) {
+      this.paginaActualDonGustavo++;
+    }
+  }
+
+  // Obtener los elementos de la página actual para David Rio
+  get elementosPaginaDavidRio() {
+    const inicio = (this.paginaActualDavidRio - 1) * this.tamañoPaginaDavidRio;
+    const fin = inicio + this.tamañoPaginaDavidRio;
+    return this.polvosDavidRio.slice(inicio, fin);
+  }
+
+  // Obtener las páginas para David Rio
+  get paginasDavidRio() {
+    const totalPaginas = Math.ceil(this.polvosDavidRio.length / this.tamañoPaginaDavidRio);
+    return Array(totalPaginas).fill(0).map((_, index) => index + 1);
+  }
+
+  // Cambiar página para David Rio
+  paginaAnteriorDavidRio() {
+    if (this.paginaActualDavidRio > 1) {
+      this.paginaActualDavidRio--;
+    }
+  }
+
+  paginaSiguienteDavidRio() {
+    if (this.paginaActualDavidRio < this.paginasDavidRio.length) {
+      this.paginaActualDavidRio++;
+    }
+  }
+
+  // Obtener los elementos de la página actual para Mocafe
+  get elementosPaginaMocafe() {
+    const inicio = (this.paginaActualMocafe - 1) * this.tamañoPaginaMocafe;
+    const fin = inicio + this.tamañoPaginaMocafe;
+    return this.polvosMocafe.slice(inicio, fin);
+  }
+
+  // Obtener las páginas para Mocafe
+  get paginasMocafe() {
+    const totalPaginas = Math.ceil(this.polvosMocafe.length / this.tamañoPaginaMocafe);
+    return Array(totalPaginas).fill(0).map((_, index) => index + 1);
+  }
+
+  // Cambiar página para Mocafe
+  paginaAnteriorMocafe() {
+    if (this.paginaActualMocafe > 1) {
+      this.paginaActualMocafe--;
+    }
+  }
+
+  paginaSiguienteMocafe() {
+    if (this.paginaActualMocafe < this.paginasMocafe.length) {
+      this.paginaActualMocafe++;
+    }
+  }
+
+  // Obtener los elementos de la página actual para CoolCapp
+  get elementosPaginaCoolCapp() {
+    const inicio = (this.paginaActualCoolCap - 1) * this.tamañoPaginaCoolCap;
+    const fin = inicio + this.tamañoPaginaCoolCap;
+    return this.polvosCoolCap.slice(inicio, fin);
+  }
+
+  // Obtener las páginas para CoolCapp
+  get paginasCoolCapp() {
+    const totalPaginas = Math.ceil(this.polvosCoolCap.length / this.tamañoPaginaCoolCap);
+    return Array(totalPaginas).fill(0).map((_, index) => index + 1);
+  }
+
+  // Cambiar página para CoolCapp
+  paginaAnteriorCoolCapp() {
+    if (this.paginaActualCoolCap > 1) {
+      this.paginaActualCoolCap--;
+    }
+  }
+
+  paginaSiguienteCoolCapp() {
+    if (this.paginaActualCoolCap < this.paginasCoolCapp.length) {
+      this.paginaActualCoolCap++;
+    }
+  }
+
+  // Obtener los elementos de la página actual para Cappuccinee
+  get elementosPaginaCappuccinee() {
+    const inicio = (this.paginaActualCappuccine - 1) * this.tamañoPaginaHollander;
+    const fin = inicio + this.tamañoPaginaHollander;
+    return this.polvosCappucchinee.slice(inicio, fin);
+  }
+
+  // Obtener las páginas para Cappuccinee
+  get paginasCappuccinee() {
+    const totalPaginas = Math.ceil(this.polvosCappucchinee.length / this.tamañoPaginaHollander);
+    return Array(totalPaginas).fill(0).map((_, index) => index + 1);
+  }
+
+  // Cambiar página para Cappuccinee
+  paginaAnteriorCappuccinee() {
+    if (this.paginaActualCappuccine > 1) {
+      this.paginaActualCappuccine--;
+    }
+  }
+
+  paginaSiguienteCappuccinee() {
+    if (this.paginaActualCappuccine < this.paginasCappuccinee.length) {
+      this.paginaActualCappuccine++;
+    }
+  }
+
+  // Obtener los elementos de la página actual para Hollander
+  get elementosPaginaHollander() {
+    const inicio = (this.paginaActualHollander - 1) * this.tamañoPaginaHollander;
+    const fin = inicio + this.tamañoPaginaHollander;
+    return this.polvosHollander.slice(inicio, fin);
+  }
+
+  // Obtener las páginas para Hollander
+  get paginasHollander() {
+    const totalPaginas = Math.ceil(this.polvosHollander.length / this.tamañoPaginaHollander);
+    return Array(totalPaginas).fill(0).map((_, index) => index + 1);
+  }
+
+  // Cambiar página para Hollander
+  paginaAnteriorHollander() {
+    if (this.paginaActualHollander > 1) {
+      this.paginaActualHollander--;
+    }
+  }
+
+  paginaSiguienteHollander() {
+    if (this.paginaActualHollander < this.paginasHollander.length) {
+      this.paginaActualHollander++;
+    }
+  }
+
+  ngOnInit(): void {
     this.dataService.getInsumos().then((insumosArray: any[]) => {
       this.insumosArray = insumosArray;
       console.log('Información de insumosArray', this.insumosArray);
@@ -52,7 +365,7 @@ export class PolvosComponent {
         insumo.urlArticleFirst && insumo.urlArticleFirst.trim() !== ''
       );
 
-      // Filtra los ChillOut Mix y crea un nuevo array
+      // Filtra los ChillOut JustFruit y crea un nuevo array
       this.polvosChillOutJustFruit = this.insumosArray.filter(insumo =>
         this.contienePalabrasChillOutJustFruit(insumo.name) &&
         insumo.urlArticleFirst && insumo.urlArticleFirst.trim() !== ''
@@ -88,15 +401,33 @@ export class PolvosComponent {
         insumo.urlArticleFirst && insumo.urlArticleFirst.trim() !== ''
       );
 
-       // Filtra los Mocafe crea un nuevo array
-       this.polvosMocafe = this.insumosArray.filter(insumo =>
+      // Filtra los Mocafe crea un nuevo array
+      this.polvosMocafe = this.insumosArray.filter(insumo =>
         this.contienePalabrasMocafe(insumo.name) &&
         insumo.urlArticleFirst && insumo.urlArticleFirst.trim() !== ''
       );
 
-       // Filtra los Mocafe crea un nuevo array
-       this.polvosCoolCap = this.insumosArray.filter(insumo =>
+      // Filtra los CoolCapp crea un nuevo array
+      this.polvosCoolCap = this.insumosArray.filter(insumo =>
         this.contienePalabrasCoolCapp(insumo.name) &&
+        insumo.urlArticleFirst && insumo.urlArticleFirst.trim() !== ''
+      );
+
+      // Filtra los Cappuccine crea un nuevo array
+      this.polvosCappucchinee = this.insumosArray.filter(insumo =>
+        this.contienePalabrasCappuccine(insumo.name) &&
+        insumo.urlArticleFirst && insumo.urlArticleFirst.trim() !== ''
+      );
+
+      // Filtra los Cappuccine crea un nuevo array
+      this.polvosHollander = this.insumosArray.filter(insumo =>
+        this.contienePalabrasHollander(insumo.name) &&
+        insumo.urlArticleFirst && insumo.urlArticleFirst.trim() !== ''
+      );
+      
+      // Filtra los Tea Zone crea un nuevo array
+      this.polvosTeaZone = this.insumosArray.filter(insumo =>
+        this.contienePalabrasTeaZone(insumo.name) &&
         insumo.urlArticleFirst && insumo.urlArticleFirst.trim() !== ''
       );
 
@@ -284,6 +615,63 @@ export class PolvosComponent {
 
   contienePalabrasCoolCapp(nombre: string): boolean {
     const palabrasClave = ['COOL', 'CAPP',];
+
+    // Palabras a excluir
+    const palabrasExcluidas = ['CHILLOUT', 'DAVID', 'TAPIOCA'];
+
+    // Convierte el nombre a mayúsculas para hacer la coincidencia sin distinción de mayúsculas y minúsculas
+    const nombreEnMayusculas = nombre.toUpperCase();
+
+    // Verifica que todas las palabras clave estén presentes y que ninguna palabra excluida esté presente
+    const cumpleCondicion = palabrasClave.every(palabra => nombreEnMayusculas.includes(palabra))
+      && !palabrasExcluidas.some(excluida => nombreEnMayusculas.includes(excluida));
+
+    //console.log(nombre, cumpleCondicion); // Verifica los resultados en la consola
+    return cumpleCondicion;
+
+    // Verifica que todas las palabras clave estén presentes y que ninguna palabra excluida esté presente
+  }
+
+  contienePalabrasTeaZone(nombre: string): boolean {
+    const palabrasClave = ['TEA', 'ZONE', 'POLVO'];
+
+    // Palabras a excluir
+    const palabrasExcluidas = ['CONCENTRADO', 'JELLY', 'PERLAS'];
+
+    // Convierte el nombre a mayúsculas para hacer la coincidencia sin distinción de mayúsculas y minúsculas
+    const nombreEnMayusculas = nombre.toUpperCase();
+
+    // Verifica que todas las palabras clave estén presentes y que ninguna palabra excluida esté presente
+    const cumpleCondicion = palabrasClave.every(palabra => nombreEnMayusculas.includes(palabra))
+      && !palabrasExcluidas.some(excluida => nombreEnMayusculas.includes(excluida));
+
+    //console.log(nombre, cumpleCondicion); // Verifica los resultados en la consola
+    return cumpleCondicion;
+
+    // Verifica que todas las palabras clave estén presentes y que ninguna palabra excluida esté presente
+  }
+
+  contienePalabrasCappuccine(nombre: string): boolean {
+    const palabrasClave = ['CAPPUCCINE'];
+
+    // Palabras a excluir
+    const palabrasExcluidas = ['CHILLOUT', 'DAVID', 'TAPIOCA'];
+
+    // Convierte el nombre a mayúsculas para hacer la coincidencia sin distinción de mayúsculas y minúsculas
+    const nombreEnMayusculas = nombre.toUpperCase();
+
+    // Verifica que todas las palabras clave estén presentes y que ninguna palabra excluida esté presente
+    const cumpleCondicion = palabrasClave.every(palabra => nombreEnMayusculas.includes(palabra))
+      && !palabrasExcluidas.some(excluida => nombreEnMayusculas.includes(excluida));
+
+    //console.log(nombre, cumpleCondicion); // Verifica los resultados en la consola
+    return cumpleCondicion;
+
+    // Verifica que todas las palabras clave estén presentes y que ninguna palabra excluida esté presente
+  }
+
+  contienePalabrasHollander(nombre: string): boolean {
+    const palabrasClave = ['HOLLANDER'];
 
     // Palabras a excluir
     const palabrasExcluidas = ['CHILLOUT', 'DAVID', 'TAPIOCA'];
