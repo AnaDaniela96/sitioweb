c0b37b01b3a17e1e3ead0a1604a67383bcfaf688
AnaDaniela96 <108939830+AnaDaniela96@users.noreply.github.com>
2024-09-18 14:07:49 -0600

    update "maquinas de café"

--- src/app/pages/productos-y-servicios/maquinas/cafeteras/cafeteras.component.ts
+++ src/app/pages/productos-y-servicios/maquinas/cafeteras/cafeteras.component.ts
@@ -1,5 +1,5 @@
-import { Component } from '@angular/core';
-import { DataService } from 'src/app/utils/data.service';
+import { Component, AfterViewInit, QueryList, ViewChildren } from '@angular/core';
+import { DataService } from '../../../../utils/data.service';
 import { DomSanitizer, SafeUrl } from '@angular/platform-browser';
 
 @Component({
@@ -17,20 +17,102 @@ export class CafeterasComponent {
   maquinasArray: any[] = [];
   extractedUrls: any;
 
-  cafeteras: any[] = [];
+  // Menu 
+  hidesReneka: boolean = true;
+  hidesMelitta: boolean = true;
+
+  mostrarCuadricula: boolean = false;
+  elementosVisibles: number = 0;
+
+  // Datos para maquinas RENEKA
+  cafeterasReneka: any[] = [];
+  paginaActualReneka: number = 1;
+  tamañoPaginaReneka: number = 2;
+
+  // Datos para maquinas Melitta
+  cafeterasMelitta: any[] = [];
+  paginaActualMelitta: number = 1;
+  tamañoPaginaMelitta: number = 2;
+
+  @ViewChildren('tarjeta')
+  tarjetas!: QueryList<any>;
+
+  ngAfterViewInit() {
+    this.elementosVisibles = this.tarjetas.length;
+  }
+
+  // Obtener los elementos de la página actual para Reneka
+  get elementosPaginaReneka() {
+    const inicio = (this.paginaActualReneka - 1) * this.tamañoPaginaReneka;
+    const fin = inicio + this.tamañoPaginaReneka;
+    return this.cafeterasReneka.slice(inicio, fin);
+  }
+
+  // Obtener las páginas para Reneka
+  get paginasReneka() {
+    const totalPaginas = Math.ceil(this.cafeterasReneka.length / this.tamañoPaginaReneka);
+    return Array(totalPaginas).fill(0).map((_, index) => index + 1);
+  }
+
+  // Cambiar página para Reneka
+  paginaAnteriorReneka() {
+    if (this.paginaActualReneka > 1) {
+      this.paginaActualReneka--;
+    }
+  }
+
+  paginaSiguienteReneka() {
+    if (this.paginaActualReneka < this.paginasReneka.length) {
+      this.paginaActualReneka++;
+    }
+  }
+
+  // Obtener los elementos de la página actual para Melitta
+  get elementosPaginaMelitta() {
+    const inicio = (this.paginaActualMelitta - 1) * this.tamañoPaginaMelitta;
+    const fin = inicio + this.tamañoPaginaMelitta;
+    return this.cafeterasMelitta.slice(inicio, fin);
+  }
+
+  // Obtener las páginas para Melittaa
+  get paginasMelitta() {
+    const totalPaginas = Math.ceil(this.cafeterasMelitta.length / this.tamañoPaginaMelitta);
+    return Array(totalPaginas).fill(0).map((_, index) => index + 1);
+  }
+
+  // Cambiar página para Melitta
+  paginaAnteriorMelitta() {
+    if (this.paginaActualMelitta > 1) {
+      this.paginaActualMelitta--;
+    }
+  }
+
+  paginaSiguienteMelitta() {
+    if (this.paginaActualMelitta < this.paginasMelitta.length) {
+      this.paginaActualMelitta++;
+    }
+  }
 
   ngOnInit(): void {
     this.dataService.getMaquinaria().then((maquinasArray: any[]) => {
       this.maquinasArray = maquinasArray;
-      //console.log(this.maquinasArray);
+      console.log(this.maquinasArray);
 
-      //Filtrar las licuadoras
-      this.cafeteras = this.maquinasArray.filter(maquina =>
-        this.contienePalabraCafetera(maquina.name) &&
+      //Filtrar las cafeteras Reneka
+      this.cafeterasReneka = this.maquinasArray.filter(maquina =>
+        this.contienePalabraCafeteraReneka(maquina.name)
+        &&
         maquina.urlArticleFirst && maquina.urlArticleFirst.trim() !== ''
       );
 
-      console.log(this.cafeteras);
+      //Filtrar las cafeteras Melitta
+      this.cafeterasMelitta = this.maquinasArray.filter(maquina =>
+        this.contienePalabraCafeteraMelitta(maquina.name)
+        &&
+        maquina.urlArticleFirst && maquina.urlArticleFirst.trim() !== ''
+      );
+
+      //console.log(this.cafeterasReneka);
 
     }).catch((error: any) => {
       console.error('Error al obtener datos de insumo', error);
@@ -60,10 +142,17 @@ export class CafeterasComponent {
     return this.sanitizer.bypassSecurityTrustUrl(url);
   }
 
-  contienePalabraCafetera(nombre: string): boolean {
-    const palabrasClave = ['CAFETERA'];
+  contienePalabraCafeteraReneka(nombre: string): boolean {
+    const palabrasClave = ['CAFETERA', 'RENEKA'];
     const cumpleCondicion = palabrasClave.every(palabra => nombre.toUpperCase().includes(palabra));
-    console.log(nombre, cumpleCondicion); // Verifica los resultados en la consola
+    //console.log(nombre, cumpleCondicion); // Verifica los resultados en la consola
+    return cumpleCondicion;
+  }
+
+  contienePalabraCafeteraMelitta(nombre: string): boolean {
+    const palabrasClave = ['CAFETERA', 'MELITTA'];
+    const cumpleCondicion = palabrasClave.every(palabra => nombre.toUpperCase().includes(palabra));
+    //console.log(nombre, cumpleCondicion); // Verifica los resultados en la consola
     return cumpleCondicion;
   }
 
