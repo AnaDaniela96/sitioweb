7a087bdb87613ca31ccf3e57c236171e6b4a6114
AnaDaniela96 <108939830+AnaDaniela96@users.noreply.github.com>
2024-09-18 12:49:46 -0600

    Update Moleculares module

--- src/app/pages/productos-y-servicios/insumos/moleculares/moleculares.component.ts
+++ src/app/pages/productos-y-servicios/insumos/moleculares/moleculares.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, AfterViewInit, QueryList, ViewChildren } from '@angular/core';
 import { DataService } from '../../../../utils/data.service';
 import { DomSanitizer, SafeUrl } from '@angular/platform-browser';
 
@@ -17,21 +17,209 @@ export class MolecularesComponent {
   insumosArray: any[] = [];
   extractedUrls: any;
 
-  //Moleculares
-  chillOutPearls: any[] = [];
-  teaZonePerlas: any[] = [];
-  chillOutJelly: any[] = [];
-  teaZoneJelly: any[] = [];
-  chillOutTapioca: any[] = [];
-  teaZoneTapioca: any[] = [];
-
   //Menu
-hideChillOutPearls: boolean = true;
-hidesTeaZonePerlas: boolean = true;
-hidesChillOutJelly: boolean = true;
-hidesTeaZoneJelly: boolean = true;
-hidesChillOutTapioca: boolean = true;
-hidesTeaZoneTapioca: boolean = true;
+  hideChillOutPearls: boolean = true;
+  hidesTeaZonePerlas: boolean = true;
+  hidesChillOutJelly: boolean = true;
+  hidesTeaZoneJelly: boolean = true;
+  hidesChillOutTapioca: boolean = true;
+  hidesTeaZoneTapioca: boolean = true;
+
+  mostrarCuadricula: boolean = false;
+  elementosVisibles: number = 0;
+
+  //chillOut Pearls
+  chillOutPearls: any[] = [];
+  paginaActualChillOutPearls: number = 1;
+  tamañoPaginaChillOutPearls: number = 5;
+
+  //Tea Zone Pearls
+  teaZonePerlas: any[] = [];
+  paginaActualteaZonePerlas: number = 1;
+  tamañoPaginateaZonePerlas: number = 5;
+
+  //chillOutJelly
+  chillOutJelly: any[] = [];
+  paginaActualChillOutJelly: number = 1;
+  tamañoPaginaChillOutJelly: number = 5;
+
+  //teaZone Jelly
+  teaZoneJelly: any[] = [];
+  paginaActualTeaZoneJelly: number = 1;
+  tamañoPaginaTeaZoneJelly: number = 5;
+
+  //ChillOut Tapioca
+  chillOutTapioca: any[] = [];
+  paginaActualchillOutTapioca: number = 1;
+  tamañoPaginachillOutTapioca: number = 5;
+
+  //TeaZoneTapioca
+  teaZoneTapioca: any[] = [];
+  paginaActualTeaZoneTapioca: number = 1;
+  tamañoPaginaTeaZoneTapioca: number = 5;
+
+  @ViewChildren('tarjeta')
+  tarjetas!: QueryList<any>;
+
+  ngAfterViewInit() {
+    this.elementosVisibles = this.tarjetas.length;
+  }
+
+  // Obtener los elementos de la página actual para ChillOutPearls
+  get elementosPaginaChillOutPearls() {
+    const inicio = (this.paginaActualChillOutPearls - 1) * this.tamañoPaginaChillOutPearls;
+    const fin = inicio + this.tamañoPaginaChillOutPearls;
+    return this.chillOutPearls.slice(inicio, fin);
+  }
+
+  // Obtener las páginas ChillOutPearls
+  get paginasChillOutPearls() {
+    const totalPaginas = Math.ceil(this.chillOutPearls.length / this.tamañoPaginaChillOutPearls);
+    return Array(totalPaginas).fill(0).map((_, index) => index + 1);
+  }
+
+  // Cambiar página para ChillOutPearls
+  paginaAnteriorChillOutPearls() {
+    if (this.paginaActualChillOutPearls > 1) {
+      this.paginaActualChillOutPearls--;
+    }
+  }
+
+  paginaSiguienteChillOutPearls() {
+    if (this.paginaActualChillOutPearls < this.paginasChillOutPearls.length) {
+      this.paginaActualChillOutPearls++;
+    }
+  }
+
+  // Obtener los elementos de la página actual para teaZonePerlas
+  get elementosPaginaTeaZonePerlas() {
+    const inicio = (this.paginaActualteaZonePerlas - 1) * this.tamañoPaginateaZonePerlas;
+    const fin = inicio + this.tamañoPaginateaZonePerlas;
+    return this.teaZonePerlas.slice(inicio, fin);
+  }
+
+  // Obtener las páginas Tea Zone Pearls
+  get paginasTeaZonePerlas() {
+    const totalPaginas = Math.ceil(this.teaZonePerlas.length / this.tamañoPaginateaZonePerlas);
+    return Array(totalPaginas).fill(0).map((_, index) => index + 1);
+  }
+
+  // Cambiar página para Tea Zone Pearls
+  paginaAnteriorTeaZonePerlas() {
+    if (this.paginaActualteaZonePerlas > 1) {
+      this.paginaActualteaZonePerlas--;
+    }
+  }
+
+  paginaSiguienteTeaZonePerlas() {
+    if (this.paginaActualteaZonePerlas < this.paginasChillOutPearls.length) {
+      this.paginaActualteaZonePerlas++;
+    }
+  }
+
+  // Obtener los elementos de la página actual para chillOutJelly
+  get elementosPaginaChillOutJelly() {
+    const inicio = (this.paginaActualChillOutJelly - 1) * this.tamañoPaginaChillOutJelly;
+    const fin = inicio + this.tamañoPaginaChillOutJelly;
+    return this.chillOutJelly.slice(inicio, fin);
+  }
+
+  // Obtener las páginas chillOutJelly
+  get paginasChillOutJelly() {
+    const totalPaginas = Math.ceil(this.chillOutJelly.length / this.tamañoPaginaChillOutJelly);
+    return Array(totalPaginas).fill(0).map((_, index) => index + 1);
+  }
+
+  // Cambiar página para chillOutJelly
+  paginaAnteriorChillOutJelly() {
+    if (this.paginaActualChillOutJelly > 1) {
+      this.paginaActualChillOutJelly--;
+    }
+  }
+
+  paginaSiguienteChillOutJelly() {
+    if (this.paginaActualChillOutJelly < this.paginasChillOutPearls.length) {
+      this.paginaActualChillOutJelly++;
+    }
+  }
+
+  // Obtener los elementos de la página actual para teaZone Jelly
+  get elementosPaginaTeaZoneJelly() {
+    const inicio = (this.paginaActualTeaZoneJelly - 1) * this.tamañoPaginaTeaZoneJelly;
+    const fin = inicio + this.tamañoPaginaTeaZoneJelly;
+    return this.teaZoneJelly.slice(inicio, fin);
+  }
+
+  // Obtener las páginas teaZone Jelly
+  get paginasTeaZoneJelly() {
+    const totalPaginas = Math.ceil(this.teaZoneJelly.length / this.tamañoPaginaTeaZoneJelly);
+    return Array(totalPaginas).fill(0).map((_, index) => index + 1);
+  }
+
+  // Cambiar página para teaZone Jelly
+  paginaAnteriorTeaZoneJelly() {
+    if (this.paginaActualTeaZoneJelly > 1) {
+      this.paginaActualTeaZoneJelly--;
+    }
+  }
+
+  paginaSiguienteTeaZoneJelly() {
+    if (this.paginaActualTeaZoneJelly < this.paginasChillOutPearls.length) {
+      this.paginaActualTeaZoneJelly++;
+    }
+  }
+
+  // Obtener los elementos de la página actual para ChillOut Tapioca
+  get elementosPaginaChillOutTapioca() {
+    const inicio = (this.paginaActualchillOutTapioca - 1) * this.tamañoPaginachillOutTapioca;
+    const fin = inicio + this.tamañoPaginachillOutTapioca;
+    return this.chillOutTapioca.slice(inicio, fin);
+  }
+
+  // Obtener las páginas ChillOut Tapioca
+  get paginasChillOutTapioca() {
+    const totalPaginas = Math.ceil(this.chillOutTapioca.length / this.tamañoPaginachillOutTapioca);
+    return Array(totalPaginas).fill(0).map((_, index) => index + 1);
+  }
+
+  // Cambiar página para ChillOut Tapioca
+  paginaAnteriorChillOutTapioca() {
+    if (this.paginaActualchillOutTapioca > 1) {
+      this.paginaActualchillOutTapioca--;
+    }
+  }
+
+  paginaSiguienteChillOutTapioca() {
+    if (this.paginaActualchillOutTapioca < this.paginasChillOutTapioca.length) {
+      this.paginaActualchillOutTapioca++;
+    }
+  }
+
+  // Obtener los elementos de la página actual para teaZone Tapioca
+  get elementosPaginaTeaZoneTapioca() {
+    const inicio = (this.paginaActualTeaZoneTapioca - 1) * this.tamañoPaginaTeaZoneTapioca;
+    const fin = inicio + this.tamañoPaginaTeaZoneTapioca;
+    return this.teaZoneTapioca.slice(inicio, fin);
+  }
+
+  // Obtener las páginas teaZone Tapioca
+  get paginasTeaZoneTapioca() {
+    const totalPaginas = Math.ceil(this.teaZoneTapioca.length / this.tamañoPaginaTeaZoneTapioca);
+    return Array(totalPaginas).fill(0).map((_, index) => index + 1);
+  }
+
+  // Cambiar página para teaZone Tapioca
+  paginaAnteriorTeaZoneTapioca() {
+    if (this.paginaActualTeaZoneTapioca > 1) {
+      this.paginaActualTeaZoneTapioca--;
+    }
+  }
+
+  paginaSiguienteTeaZoneTapioca() {
+    if (this.paginaActualTeaZoneTapioca < this.paginasTeaZoneTapioca.length) {
+      this.paginaActualTeaZoneTapioca++;
+    }
+  }
 
   ngOnInit(): void {
     this.dataService.getInsumos().then((insumosArray: any[]) => {
@@ -92,7 +280,7 @@ hidesTeaZoneTapioca: boolean = true;
         .map(url => url);
       // console.log(urls);
       // Devuelve la primera URL encontrada (o null si no hay URLs válidas)
-      this.extractedUrls = urls.length > 0 ? urls[0] : null;
+      this.extractedUrls = urls.length > 0 ? urls[0] + '?v=1' : null;
     } else {
       this.extractedUrls = null; // No hay URLs válidas
     }
