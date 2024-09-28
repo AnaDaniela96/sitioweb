64746208a790f348a15371e7be1752f7700fed0a
AnaDaniela96 <108939830+AnaDaniela96@users.noreply.github.com>
2024-09-28 11:16:18 -0600

    update tes component

--- src/app/pages/productos-y-servicios/insumos/tes/tes.component.ts
+++ src/app/pages/productos-y-servicios/insumos/tes/tes.component.ts
@@ -21,14 +21,10 @@ export class TesComponent {
   hidesTeEurote: boolean = true;
   hidesTeStash: boolean = true;
 
-  //Tes 
-  // teEurote: any[] = [];
-  // teStash: any[] = [];
-
   mostrarCuadricula: boolean = false;
   elementosVisibles: number = 0;
 
-   // Datos para Eurote
+  // Datos para Eurote
   teEurote: any[] = [];
   paginaActualEuroTe: number = 1;
   tamañoPaginaEuroTe: number = 5;
@@ -38,6 +34,16 @@ export class TesComponent {
   paginaActualStash: number = 1;
   tamañoPaginaStash: number = 5;
 
+  // Datos para Chillout
+  teChillout: any[] = [];
+  paginaActualChillout: number = 1;
+  tamañoPaginaChillout: number = 5;
+
+  // Datos para PranaChai
+  tePranaChai: any[] = [];
+  paginaActualPranaChai: number = 1;
+  tamañoPaginaPranaChai: number = 5;
+
   @ViewChildren('tarjeta')
   tarjetas!: QueryList<any>;
 
@@ -45,7 +51,7 @@ export class TesComponent {
     this.elementosVisibles = this.tarjetas.length;
   }
 
-    // Obtener los elementos de la página actual para EuroTe
+  // Obtener los elementos de la página actual para EuroTe
   get elementosPaginaTeEuroTe() {
     const inicio = (this.paginaActualEuroTe - 1) * this.tamañoPaginaEuroTe;
     const fin = inicio + this.tamañoPaginaEuroTe;
@@ -97,7 +103,57 @@ export class TesComponent {
     }
   }
 
+  // Obtener los elementos de la página actual para Chillout
+  get elementosPaginaTeChillout() {
+    const inicio = (this.paginaActualChillout - 1) * this.tamañoPaginaChillout;
+    const fin = inicio + this.tamañoPaginaChillout;
+    return this.teChillout.slice(inicio, fin);
+  }
 
+  // Obtener las páginas para Chillout
+  get paginasChillout() {
+    const totalPaginas = Math.ceil(this.teChillout.length / this.tamañoPaginaChillout);
+    return Array(totalPaginas).fill(0).map((_, index) => index + 1);
+  }
+
+  // Cambiar página para para Chillout
+  paginaAnteriorChillout() {
+    if (this.paginaActualChillout > 1) {
+      this.paginaActualChillout--;
+    }
+  }
+
+  paginaSiguienteChillout() {
+    if (this.paginaActualChillout < this.paginasChillout.length) {
+      this.paginaActualChillout++;
+    }
+  }
+
+    // Obtener los elementos de la página actual para PranaChai
+    get elementosPaginaTePranaChai() {
+      const inicio = (this.paginaActualPranaChai - 1) * this.tamañoPaginaPranaChai;
+      const fin = inicio + this.tamañoPaginaPranaChai;
+      return this.tePranaChai.slice(inicio, fin);
+    }
+  
+    // Obtener las páginas para PranaChai
+    get paginasPranaChai() {
+      const totalPaginas = Math.ceil(this.tePranaChai.length / this.tamañoPaginaPranaChai);
+      return Array(totalPaginas).fill(0).map((_, index) => index + 1);
+    }
+  
+    // Cambiar página para para PranaChai
+    paginaAnteriorPranaChai() {
+      if (this.paginaActualPranaChai > 1) {
+        this.paginaActualPranaChai--;
+      }
+    }
+  
+    paginaSiguientePranaChai() {
+      if (this.paginaActualPranaChai < this.paginasPranaChai.length) {
+        this.paginaActualPranaChai++;
+      }
+    }
 
   ngOnInit(): void {
     this.dataService.getInsumos().then((insumosArray: any[]) => {
@@ -115,6 +171,21 @@ export class TesComponent {
         this.contienePalabrasTeStash(insumo.name) &&
         insumo.urlArticleFirst && insumo.urlArticleFirst.trim() !== ''
       );
+
+      // Filtra los Te Stash y crea un nuevo array
+      this.teChillout = this.insumosArray.filter(insumo =>
+        this.contienePalabrasTeChillout(insumo.name) 
+        // &&
+        // insumo.urlArticleFirst && insumo.urlArticleFirst.trim() !== ''
+      );
+
+      // Filtra los Te Stash y crea un nuevo array
+      this.tePranaChai = this.insumosArray.filter(insumo =>
+        this.contienePalabrasTePranaChai(insumo.name) 
+        // &&
+        // insumo.urlArticleFirst && insumo.urlArticleFirst.trim() !== ''
+      );
+
     }).catch((error: any) => {
       console.error('Error al obtener datos de insumo', error);
     })
@@ -167,6 +238,44 @@ export class TesComponent {
 
     // Palabras a excluir
     const palabrasExcluidas = ['TISANA', 'EUROTE', 'DESCAFEINADO'];
+
+    // Convierte el nombre a mayúsculas para hacer la coincidencia sin distinción de mayúsculas y minúsculas
+    const nombreEnMayusculas = nombre.toUpperCase();
+
+    // Verifica que todas las palabras clave estén presentes y que ninguna palabra excluida esté presente
+    const cumpleCondicion = palabrasClave.every(palabra => nombreEnMayusculas.includes(palabra))
+      && !palabrasExcluidas.some(excluida => nombreEnMayusculas.includes(excluida));
+
+    //console.log(nombre, cumpleCondicion); // Verifica los resultados en la consola
+    return cumpleCondicion;
+
+    // Verifica que todas las palabras clave estén presentes y que ninguna palabra excluida esté presente
+  }
+
+  contienePalabrasTeChillout(nombre: string): boolean {
+    const palabrasClave = ['TE', 'CHILLOUT', 'BOLSA', '50'];
+
+    // Palabras a excluir
+    const palabrasExcluidas = ['TISANA', 'CURCUMA', 'DESCAFEINADO'];
+
+    // Convierte el nombre a mayúsculas para hacer la coincidencia sin distinción de mayúsculas y minúsculas
+    const nombreEnMayusculas = nombre.toUpperCase();
+
+    // Verifica que todas las palabras clave estén presentes y que ninguna palabra excluida esté presente
+    const cumpleCondicion = palabrasClave.every(palabra => nombreEnMayusculas.includes(palabra))
+      && !palabrasExcluidas.some(excluida => nombreEnMayusculas.includes(excluida));
+
+    //console.log(nombre, cumpleCondicion); // Verifica los resultados en la consola
+    return cumpleCondicion;
+
+    // Verifica que todas las palabras clave estén presentes y que ninguna palabra excluida esté presente
+  }
+
+  contienePalabrasTePranaChai(nombre: string): boolean {
+    const palabrasClave = ['PRANA', 'CHAI'];
+
+    // Palabras a excluir
+    const palabrasExcluidas = ['OLLA', 'ACCESORIO'];
 
     // Convierte el nombre a mayúsculas para hacer la coincidencia sin distinción de mayúsculas y minúsculas
     const nombreEnMayusculas = nombre.toUpperCase();
